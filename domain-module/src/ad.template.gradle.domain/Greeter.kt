c2bd016b3fc76aebf4096a95291fe9cbff311399
Andrej Dyck <andrej.dyck@rwth-aachen.de>
2018-05-31 12:07:16 +0200

    Add capability to greet all

--- domain-module/src/ad.template.gradle.domain/Greeter.kt
+++ domain-module/src/ad.template.gradle.domain/Greeter.kt
@@ -3,9 +3,12 @@ package ad.template.gradle.domain
 interface Greeter {
     
     fun greet(whom: String): String
+    
+    fun greetAll(whom: List<String>): List<String> =
+        whom.map { greet(it) }
 }
 
 class EnglishGreeter : Greeter {
     
-    override fun greet(whom: String): String  = "Hello, $whom!"
+    override fun greet(whom: String): String  = "Hello, $whom! :D"
 }
