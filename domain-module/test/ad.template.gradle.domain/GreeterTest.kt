90d28085635b386320631796694a5cd2242a26f5
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2018-05-31 12:07:16 +0200

    Add capability to greet all

--- domain-module/test/ad.template.gradle.domain/GreeterTest.kt
+++ domain-module/test/ad.template.gradle.domain/GreeterTest.kt
@@ -6,16 +6,24 @@ import org.junit.jupiter.api.*
 @DisplayName("Greeter")
 class GreeterTest {
     
+    private val greeter = EnglishGreeter()
+    
     @Test
     fun `must greet the world!`() {
-        // given
-        val greeter = EnglishGreeter()
-        
-        // when
         val greeting = greeter.greet(WORLD)
         
-        // then
-        assertThat(greeting).isEqualTo("Hello, $WORLD!")
+        assertThat(greeting).startsWith("Hello, $WORLD!")
+    }
+    
+    @Test
+    fun `must greet everyone!`() {
+        val greetings = greeter.greetAll(listOf("Alice", "Bob", "Charlie"))
+        
+        assertThat(greetings).containsExactly(
+            "Hello, Alice! :D",
+            "Hello, Bob! :D",
+            "Hello, Charlie! :D"
+        )
     }
 }
 
