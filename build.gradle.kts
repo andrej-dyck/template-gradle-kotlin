7fab4c619111382e60f04654066cee3d88298b75
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2023-01-10 13:46:29 +0100

    Bump detekt version and update its config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.0"
-    id("io.gitlab.arturbosch.detekt") version "1.21.0"
+    id("io.gitlab.arturbosch.detekt") version "1.22.0"
 }
 
 group = "ad.template.gradle.kotlin"
