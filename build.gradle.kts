046ee6ffc93a6d964e97ead4a4fedecde09e99cc
Andrej Dyck <43913051+andrej-dyck@users.noreply.github.com>
2023-08-19 11:14:03 +0200

    Bump detekt to 1.23.1 and update it's config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.0"
-    id("io.gitlab.arturbosch.detekt") version "1.22.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
 }
 
 group = "ad.template.gradle.kotlin"
